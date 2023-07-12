07e3586411dfc03cb6f87d667a4669e1c1c8e612
David Steadman <wil.steadman@icloud.com>
2023-07-12 11:43:05 -0400

    Setup function definitions

---
+++ chip8.c
@@ -0,0 +1,19 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdint.h>
+
+Chip8 *init_chip8(void) {
+
+}
+
+void load_game(const char *filename) {
+
+}
+
+void read_instruction(void) {
+
+}
+
+void parse_instruction(void) {
+
+}
--- chip8.h
+++ chip8.h
@@ -1,6 +1,39 @@
 #ifndef CHIP8_H
 #define CHIP8_H
 
+/* Chip8 internal emulator state */
+typedef struct {
+    uint8_t ram[0xFF];
+    uint8_t V[0xF]; // Registers
+    uint16_t pc;    // Program counter
+    Instruction instruction;
 
+    uint8_t sp;     // Stack pointer
+    uint16_t stack[16];
+
+    bool *keyboard; // 16 characters
+    bool *display;
+
+    uint8_t delay_timer;
+    uint8_t sound_time;
+} Chip8;
+
+/* Instruction components */
+// NOTE: no use of unions here because values are store big-endian
+typedef struct {
+    uint16_t nnn;   // Lowest 12 bits of instruction
+    uint8_t n;      // Lowest 4 bits of instruction
+    uint8_t x;      // Lowest 4 bits of high-byte of instruction
+    uint8_t y;      // Highest 4 bits of low-byte of instruction
+    uint8_t kk;     // Lowest 8 bits of instruction
+} Instruction;
+
+Chip8 *init_chip8(void);
+void load_game(const char *filename);
+void read_instruction(void);
+void parse_instruction(void);
+
+// Extern variables
+extern Chip8 *chip8;
 
 #endif
